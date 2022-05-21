f74d7e3b0211c9e6ff7421a91a254fd65300d891
namotoy <namotopet0615@gmail.com>
2022-05-21 12:10:42 +0900

    関数を使った書き換え

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 'use strict'
 
 {
-// const source = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-const source = [];
-for (let i = 0; i < 15; i++){
-  source[i] = i + 1; 
-}
-const b = [];
-// b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-// b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-// b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-// b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-// b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+function createColumn(col){
+  const source = [];
+  for (let i = 0; i < 20; i++){
+    source[i] = i + 1; 
+  }
+  const b = [];
+  for(let i = 0; i < 5; i++){
+    b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+  }
+  console.log(b);
+  }
 
-for(let i = 0; i < 5; i++){
-  b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-
-console.log(b);
+  const columns = [];
+  columns[0] = createColumn(0);
+  columns[1] = createColumn(1);
+  columns[2] = createColumn(2);
+  columns[3] = createColumn(3);
+  columns[4] = createColumn(4);
 }
